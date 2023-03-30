e4de8623b6c6a2c44c1432629d235376c1ee4801
paulson05 <onyebuchiokofu@gmail.com>
2023-03-30 01:21:45 +0100

    added department

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Livewire\Department;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,17 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-
-Route::get('/department', Department::class);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
 });
